20d26a7127c256884ef677eb81d873c69ea374ae
Nuriddin <Nuriddin.Obidjonov777@gmail.com>
2022-12-04 14:02:42 +0500

    Completed maps section

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,5 +1,55 @@
 package maps
 
-func Search(dictionary map[string]string, word string) string {
-	return dictionary[word]
+const (
+	ErrNotFound         = DictionaryErr("could not find the word you were looking for")
+	ErrWordExists       = DictionaryErr("given word already exists in dictionary")
+	ErrWordDoesNotExist = DictionaryErr("given word does not exist in dictionary")
+)
+
+type DictionaryErr string
+
+func (d DictionaryErr) Error() string {
+	return string(d)
+}
+
+type Dictionary map[string]string
+
+func (d Dictionary) Search(word string) (string, error) {
+	value, ok := d[word]
+	if !ok {
+		return "", ErrNotFound
+	}
+	return value, nil
+}
+
+func (d Dictionary) Add(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		d[word] = definition
+	case nil:
+		return ErrWordExists
+	default:
+		return err
+	}
+	return nil
+}
+
+func (d Dictionary) Update(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		return ErrWordDoesNotExist
+	case nil:
+		d[word] = definition
+		return nil
+	default:
+		return err
+	}
+}
+
+func (d Dictionary) Delete(word string) {
+	delete(d, word)
 }
